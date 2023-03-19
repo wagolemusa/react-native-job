219c7e76ac7c706741b6baa397e6869b151876a3
wagolemusa <homiemusa@gmail.com>
2023-03-20 00:54:11 +0300

    Setup react-native enviroment

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Home Refuge wise</Text>
+        </View>
+    )
+}
+
+export default Home;
